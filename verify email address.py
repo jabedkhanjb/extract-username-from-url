8197b11ba03801e9af9a18edc8ea4fe0a0223e23
Jabed Khan <86038917+jabedkhanjb@users.noreply.github.com>
2023-01-19 14:47:59 +0600

    Add files via upload

---
+++ verify email address.py
@@ -0,0 +1,9 @@
+import re
+
+email = input("Enter your email: \n").strip()
+if re.search(r"^\w.+@\w+\.(\w+\.)?(com|edu|bd|uk)$", email, re.IGNORECASE):
+    print("Valid Email")
+else:
+    print("Invalid Email")
+
+# new line added
